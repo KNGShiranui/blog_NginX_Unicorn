45af2ac98ed8feb2346961a76bf83a11b63ed2fe
kengo <mathematics9973@gmail.com>
2023-03-20 16:18:54 +0900

    minor fix

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
 server '15.152.249.222', user: 'app', roles: %w{app db web}
-set :ssh_options, keys: '\\wsl.localhost\Ubuntu\home\kengo\.ssh\id_rsa'
+set :ssh_options, keys: '/home/kengo/.ssh/id_rsa'
