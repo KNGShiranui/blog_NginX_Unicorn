086a07ae3fbaab38a8d396f3f9eb3a736fba8b94
kengo <mathematics9973@gmail.com>
2023-03-20 14:35:28 +0900

    inor fix

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
 server '15.152.249.222', user: 'app', roles: %w{app db web}
-set :ssh_options, keys: '"\\wsl.localhost\Ubuntu\home\kengo\.ssh\achieve_key.pem"'
+set :ssh_options, keys: '\\wsl.localhost\Ubuntu\home\kengo\.ssh\achieve_key.pem'
